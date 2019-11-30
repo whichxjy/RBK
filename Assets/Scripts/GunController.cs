2a1b221d6cf93ad323695ef8b968c381e126a245
whichxjy <whichxjy@gmail.com>
2019-11-30 21:13:17 +0800

    Init

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class GunController : MonoBehaviour
+{
+    public ParticleSystem bulletLauncher;
+
+    private SteamVR_Action_Boolean shootAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Player", "Shoot");
+    private SteamVR_Action_Boolean switchWeaponAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Player", "SwitchWeapon");
+
+    private SteamVR_Input_Sources leftHand = SteamVR_Input_Sources.LeftHand;
+    private SteamVR_Input_Sources rightHand = SteamVR_Input_Sources.RightHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletLauncher = GetComponentInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Debug.Log(switchWeaponAction.state);
+       // Debug.Log(shootAction.state);
+
+
+        if (shootAction[leftHand].stateDown)
+        {
+            Debug.Log(1);
+            bulletLauncher.Emit(1);
+        }
+        if (switchWeaponAction[rightHand].stateDown)
+        {
+            Debug.Log(1);
+            bulletLauncher.Emit(1);
+        }
+    }
+}
---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public LayerMask layer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1, layer)) { 
+            Destroy(hit.transform.gameObject);
+        }
+    }
+}
